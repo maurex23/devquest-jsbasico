06298f930f39fb97434aec7c7b0254087c22eca9
Mauro Viana <maurofviana23@gmail.com>
2022-06-08 00:42:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function soma(num1, num2) {
+    return num1 + num2
+}
+function subtracao(num1, num2) {
+    return num1 - num2
+}
+function multiplicacao(num1, num2) {
+    return num1 * num2
+}
+function divisao(num1, num2) {
+    return num1 / num2
+}
+
+a = soma(5, 5)
+b = subtracao(5, 5) 
+c = multiplicacao(5, 5)
+d = divisao(1, 2)
+
+console.log([a, b, c, d])
